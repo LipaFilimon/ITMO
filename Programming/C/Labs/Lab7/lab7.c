450fd7738df36a657095431a6588f37dc1b35627
Lipa.Filimonov <soviet.lipa@gmail.com>
2017-11-30 16:38:36 +0300

    File lab.7 has been edited

--- Programming/C/Labs/Lab7/lab7.c
+++ Programming/C/Labs/Lab7/lab7.c
@@ -29,6 +29,22 @@ void length_LineSegment (int A,int B)
 	printf("Length of line segment AB is %d \n", length);	
 }
 
+union Modem 
+{
+    unsigned short status;
+    struct 
+    {
+        unsigned short DSL : 1;
+        unsigned short PPP : 1;
+        unsigned short Link : 1;
+    } bitStatus;
+} modem;
+
+char *sModem (int status) 
+{
+    return status == 1 ? "On" : "Off";
+}
+
 
 int main ()
 {
@@ -53,8 +69,12 @@ int main ()
     printf("\t\t\t Task 3 \n\n");
 
 
+    printf("Enter modem status (hex): ");
+    scanf("%x", &modem.status);
 
-
+    printf("DSL is %s\n", sModem(modem.bitStatus.DSL));
+    printf("PPP is %s\n", sModem(modem.bitStatus.PPP));
+    printf("Link is %s\n", sModem(modem.bitStatus.Link));
 
     getchar();
 	return 0;
